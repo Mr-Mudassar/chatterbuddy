070e029f137e6a874f6b013c104bc141b759da6f
Muhammad Mudassar <mudasir@setubo.com>
2025-10-02 01:42:38 +0500

    Loader size changed

--- src/Components/LoadingScreen/index.jsx
+++ src/Components/LoadingScreen/index.jsx
@@ -1,8 +1,8 @@
 import Loader from "@/Assets/loader.gif";
 
 const LoadingScreen = () => (
-  <div className="fixed inset-0 flex items-center justify-center !z-[50] bg-black/10">
-    <img alt="loader" src={Loader} className="w-44 h-44" />
+  <div className="fixed inset-0 flex items-center justify-center !z-[50] bg-black/20">
+    <img alt="loader" src={Loader} className="w-26 h-26" />
   </div>
 );
 
